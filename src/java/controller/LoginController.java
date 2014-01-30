e48b9ec44252025fd08b27e3ca4dc36323bac5cd
Mateo <mateodurante@gmail.com>
2014-01-30 19:01:51 -0300

    login completo

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -10,6 +10,8 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import java.util.Map;
+import model.dao.UsuarioDAO;
+import model.dao.UsuarioDAOImpl;
 import model.entities.Usuario;
 import org.apache.struts2.interceptor.SessionAware;
 
@@ -19,21 +21,26 @@ import org.apache.struts2.interceptor.SessionAware;
  */
 public class LoginController extends ActionSupport implements SessionAware, ModelDriven<Usuario>{
     private final Usuario u = new Usuario();
+    private final UsuarioDAO uDAO = new UsuarioDAOImpl();
     private Map<String, Object> sessionAttributes = ActionContext.getContext().getSession();
     private String siguiente;
     
     public String getSiguiente(){
         return siguiente;
     }
+    
+    @Override
+    public void validate(){
+    }
 
     @Override
-    public String execute(){
-        if("admin".equals(u.getMail()) && "admin".equals(u.getClave())){
-            u.setNombre("admin admin");
+    public String execute() {
+        if(uDAO.getUsuario(u) != null){
             sessionAttributes.put("usuario", u);
             siguiente = (String) sessionAttributes.get("paginaSiguiente");
             return SUCCESS;
         }
+        addFieldError("usuarioInexistente",getText("user.notExists"));
         return INPUT;
     }
  
--- src/java/model/dao/UsuarioDAOImpl.java
+++ src/java/model/dao/UsuarioDAOImpl.java
@@ -9,6 +9,7 @@ package model.dao;
 import java.util.ArrayList;
 import model.entities.Usuario;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -44,6 +45,39 @@ public class UsuarioDAOImpl implements UsuarioDAO {
             return (ArrayList<Usuario>)sessionLocal.createQuery("from Usuario").list();
         }
         catch(HibernateException e){
+            return null;
+        }
+    }
+    
+    @Override
+    public Usuario usuarioConMail(String mail) {
+        try{
+            Session sessionLocal;
+            sessionLocal=HibernateUtil.getSessionFactory().getCurrentSession();
+            return (Usuario)sessionLocal.createQuery("from Usuario where mail = ?").setString(0, mail).list().get(0);
+        }
+        catch(HibernateException e){
+            return null;
+        }
+    }
+    
+    @Override
+    public Usuario getUsuario(Usuario u) {
+        try{
+            session= HibernateUtil.getSessionFactory().getCurrentSession();
+            transaction= session.beginTransaction();
+            String hql = "from Usuario u where u.mail = :mail and u.clave = :clave";
+            Usuario e= (Usuario)session
+                    .createQuery(hql)
+                    .setString("mail",u.getMail())
+                    .setString("clave",u.getClave())
+                    .uniqueResult();
+            transaction.commit();
+            return e;
+        }
+        catch(HibernateException e){
+            if(transaction != null)
+                transaction.rollback();
             return null;
         }
     }
